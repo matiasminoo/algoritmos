0c55ff9cc9fb448d9c8f0203ad67ede6e44693bf
Matiasmiño <108136043+matiasminoo@users.noreply.github.com>
2023-05-04 19:52:08 -0300

    Update ej4.c

--- ej4.c
+++ ej4.c
@@ -2,14 +2,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int nose(int a) {
-    return 
-}
 
 int main(int argc, char*argv[]) {
  int a = atoi(argv[1]);
     int b = atoi(argv[2]);
     bool same = a == b;
-    printf(%sn, same ? 1 : 0);
+    printf("%s\n", same ? 1 : 0);
     return 0;
 }
