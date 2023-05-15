b63589ac5a554ba26528b309c6540c6d3ad97e11
mati <mati.mino2006@gmail.com>
2023-05-15 13:11:05 -0300

    finalizo ejercicio 7

--- ej7.c
+++ ej7.c
@@ -18,16 +18,33 @@ int randomNum2() {
     int randomNumCeroaDiez = rand() % 11;
     return randomNumCeroaDiez;
 }
-
-
-
-
+int randomNum3() {
+    int random = rand();
+    return random;
+}
+int randomNum4(int a, int b) {
+    int rango = b - a; 
+    int aleatorio = rand() % rango + a;  
+    return aleatorio;
+}
+int randomNum5(int a, int b) {
+    int aleatorio = rand() % (b - a + 1) + a;  
+    return aleatorio;
+}
 int main() {
-    int resultado = generarNumeroAleatorio();
-    int b = randomNum();
-    int c = randomNum2();
-    printf("Número aleatorio: %d\n", resultado);
-    printf("numero random entre cero y cien: %d\n", b);
-    printf("numero random entre cero y diez %d\n", c);
+    int a = 1;
+    int b = 3;
+    int a7 = generarNumeroAleatorio();
+    int b7 = randomNum();
+    int c7 = randomNum2();
+    int d7 = randomNum3();
+    int e7 = randomNum4(a,b);
+    int f7 = randomNum5(a,b);
+    printf("Número aleatorio: %d\n", a7);
+    printf("numero random entre cero y cien: %d\n", b7);
+    printf("numero random entre cero y diez %d\n", c7);
+    printf("numero random entre 0 y n: %d\n", d7);
+    printf("numero random entre a y b: %d\n", e7);
+    printf("numero random entre a y b incluidos los mismos: %d\n", f7);
     return 0;
 }
